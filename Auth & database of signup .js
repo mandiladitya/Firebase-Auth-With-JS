8ba21315dd64b534418867cee629dfdff32b2ea0
Aditya Mandil <46640551+mandiladitya@users.noreply.github.com>
2019-05-25 22:22:00 +0530

    Create Auth & database of signup .js

---
+++ Auth & database of signup .js
@@ -0,0 +1,79 @@
+var database = firebase.database();
+function login(){
+
+  var userEmail = document.getElementById("email").value;
+  var userPass = document.getElementById("password").value;
+
+  firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+
+    window.alert("Error : " + errorMessage);
+
+    // ...
+    var user = firebase.auth().currentUser;
+    if (user) {
+        console.log(user);
+        window.location("login.html");
+    } else {
+      window.location("firebase_test.html");
+    }
+  });
+ 
+}
+function handlesignup() {
+  var email = document.getElementById('email').value;
+  var password = document.getElementById('password').value;
+  if (email.length < 4) {
+    alert('Please enter an email address.');
+    return;
+  }
+  if (password.length < 4) {
+    alert('Please enter a password.');
+    return;
+  }
+  firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // [START_EXCLUDE]
+    if (errorCode == 'auth/weak-password') {
+      alert('The password is too weak.');
+    } else {
+      alert(errorMessage);
+    }
+    console.log(error);
+    // [END_EXCLUDE]
+  });
+  // [END createwithemail]
+}
+
+function writeuser() {
+var email=document.getElementById("email").value;
+var password=document.getElementById("password").value;
+var database = firebase.database();
+console.log(database);
+
+  firebase.database().ref('Signup/').push().set({
+    Email:email,
+    Password:password,
+    });
+    window.alert("ThankYou For Signup");
+}
+
+
+function logout(){
+  firebase.auth().signOut();
+}
+
+function sendEmailVerification() {
+  // [START sendemailverification]
+  firebase.auth().currentUser.sendEmailVerification().then(function() {
+    // Email Verification sent!
+    // [START_EXCLUDE]
+    alert('Email Verification Sent!');
+    // [END_EXCLUDE]
+  });
+  // [END sendemailverification]
+}
