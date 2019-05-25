0bd8f48b05f4bd335170ccbcfa40f89cb52fd413
Aditya Mandil <46640551+mandiladitya@users.noreply.github.com>
2019-05-25 20:17:13 +0530

    Create email-password.js

---
+++ email-password.js
@@ -0,0 +1,70 @@
+function login(){
+
+  var userEmail = document.getElementById("email").value;
+  var userPass = document.getElementById("password").value;
+
+  firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+
+    window.alert("Error : " + errorMessage);
+
+    // ...
+    var user = firebase.auth().currentUser;
+    if (user) {
+        console.log(user);
+        window.location("login.html");
+    } else {
+      window.location("firebase_test.html");
+    }
+  });
+ 
+}
+function handlesignup() {
+  var email = document.getElementById('email').value;
+  var password = document.getElementById('password').value;
+  if (email.length < 4) {
+    alert('Please enter an email address.');
+    return;
+  }
+  if (password.length < 4) {
+    alert('Please enter a password.');
+    return;
+  }
+  firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // [START_EXCLUDE]
+    if (errorCode == 'auth/weak-password') {
+      alert('The password is too weak.');
+    } else {
+      alert(errorMessage);
+    }
+    console.log(error);
+    // [END_EXCLUDE]
+  });
+  // [END createwithemail]
+}
+function logout(){
+  firebase.auth().signOut();
+}
+function sendEmailVerification() {
+  // [START sendemailverification]
+  firebase.auth().currentUser.sendEmailVerification().then(function() {
+    // Email Verification sent!
+    // [START_EXCLUDE]
+    alert('Email Verification Sent!');
+    // [END_EXCLUDE]
+  });
+  // [END sendemailverification]
+}
+
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    window.open("login.html")
+  } else {
+    // No user is signed in.
+  }
+});
